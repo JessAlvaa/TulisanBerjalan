4e56c54f26ca82deecb90ce2899a24026578f4f9
JessAlvaa <122151811+JessAlvaa@users.noreply.github.com>
2023-09-20 23:16:56 +0700

    Add files via upload

---
+++ TulisanBerjalan/TulisanBerjalan/TulisanBerjalan/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace TulisanBerjalan
+{
+    partial class frmTulisanBerjalan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblAnimasi = new System.Windows.Forms.Label();
+            this.btnKeluar = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblAnimasi
+            // 
+            this.lblAnimasi.AutoSize = true;
+            this.lblAnimasi.Font = new System.Drawing.Font("HoloLens MDL2 Assets", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnimasi.Location = new System.Drawing.Point(140, 54);
+            this.lblAnimasi.Name = "lblAnimasi";
+            this.lblAnimasi.Size = new System.Drawing.Size(122, 20);
+            this.lblAnimasi.TabIndex = 0;
+            this.lblAnimasi.Text = "Jessica Alva N";
+            this.lblAnimasi.Click += new System.EventHandler(this.lblAnimasi_Click);
+            // 
+            // btnKeluar
+            // 
+            this.btnKeluar.Location = new System.Drawing.Point(273, 126);
+            this.btnKeluar.Name = "btnKeluar";
+            this.btnKeluar.Size = new System.Drawing.Size(75, 23);
+            this.btnKeluar.TabIndex = 1;
+            this.btnKeluar.Text = "Keluar";
+            this.btnKeluar.UseVisualStyleBackColor = true;
+            this.btnKeluar.Click += new System.EventHandler(this.btnKeluar_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // frmTulisanBerjalan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 176);
+            this.Controls.Add(this.btnKeluar);
+            this.Controls.Add(this.lblAnimasi);
+            this.Name = "frmTulisanBerjalan";
+            this.Text = "Tulisan Berjalan";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAnimasi;
+        private System.Windows.Forms.Button btnKeluar;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
